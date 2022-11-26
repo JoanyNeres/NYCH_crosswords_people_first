3dd1182f78e4bd758d1fe347ddb93ffc6738fee8
joanyneres <joanybneres@gmail.com>
2022-11-25 20:06:13 -0500

    add table and clues

--- src/App.js
+++ src/App.js
@@ -1,24 +1,227 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <table>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><sup>7</sup><input type="text" name="1.P_PERSON" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><sup>1/5</sup><input type="text" name="1.C_COMPONENTS" /></td>
+          <td><input type="text" name="2.O_COMPONENTS" /></td>
+          <td><input type="text" name="3.M_COMPONENTS" /></td>
+          <td><input type="text" name="4.P_COMPONENTS" /></td>
+          <td><input type="text" name="5.O_COMPONENTS" /></td>
+          <td><input type="text" name="6.N_COMPONENTS" /></td>
+          <td><input type="text" name="7.E_COMPONENTS" /></td>
+          <td><input type="text" name="8.N_COMPONENTS" /></td>
+          <td><input type="text" name="9.T_COMPONENTS" /></td>
+          <td><input type="text" name="10.S_COMPONENTS" /></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><input type="text" name="2.O_CONTEXT" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><input type="text" name="3.R_PERSON" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><input type="text" name="3.N_CONTEXT" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><sup>6</sup><input type="text" name="1.D_DECISIONS" /></td>
+          <td></td>
+          <td><input type="text" name="4.S_PERSON" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td><sup>2</sup><input type="text" name="1.E_EMPATHIZE" /></td>
+          <td><sup>4</sup><input type="text" name="2.M_EMPATHIZE" /></td>
+          <td><input type="text" name="3.P_EMPATHIZE" /></td>
+          <td><input type="text" name="4.A_EMPATHIZE" /></td>
+          <td><input type="text" name="5.T_EMPATHIZE" /></td>
+          <td><input type="text" name="6.H_EMPATHIZE" /></td>
+          <td><input type="text" name="7.I_EMPATHIZE" /></td>
+          <td><input type="text" name="8.Z_EMPATHIZE" /></td>
+          <td><input type="text" name="9.E_EMPATHIZE" /></td>
+          <td></td>
+          <td><input type="text" name="5.O_PERSON" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input type="text" name="2.O_MODIFY" /></td>
+          <td></td>
+          <td></td>
+          <td><input type="text" name="5.E_CONTEXT" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><input type="text" name="3.C_DECISIONS" /></td>
+          <td></td>
+          <td><input type="text" name="6.N_PERSON" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input type="text" name="3.D_MODIFY" /></td>
+          <td></td>
+          <td></td>
+          <td><input type="text" name="6.X_CONTEXT" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><input type="text" name="4.I_DECISIONS" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input type="text" name="4.I_MODIFY" /></td>
+          <td></td>
+          <td></td>
+          <td><input type="text" name="7.T_CONTEXT" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><input type="text" name="5.S_DECISIONS" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input type="text" name="5.F_MODIFY" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><input type="text" name="6.I_DECISIONS" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><input type="text" name="6.Y_MODIFY" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><input type="text" name="7.O_DECISIONS" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><input type="text" name="8.N_DECISIONS" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><sup>3</sup><input type="text" name="1.F_FIRST" /></td>
+          <td><input type="text" name="2.I_FIRST" /></td>
+          <td><input type="text" name="3.R_FIRST" /></td>
+          <td><input type="text" name="4.S_FIRST" /></td>
+          <td><input type="text" name="5.T_FIRST" /></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+
+      </table>
+
+      <h2 className="clues_title">Clues</h2>
+      <ol className="clues_itens">
+        <li>Fictional name, photo, age, occupation, goals and skills are common ________ included in personas.</li>
+        <li>Personas are representations of your clients that will help you ________ with them.</li>
+        <li>Personas can be your reminder that people should come ________ before technology.</li>
+        <li>It's important to get feedback from you colleagues and ________ personas.</li>
+        <li>The key pieces of information that go to your personas depends on the ________ of the experience you want to create.</li>
+        <li>Personas can help you make ________ through the eyes of your target participants.</li>
+        <li>A persona represents a particular group and not a single ________.</li>
+      </ol>
+
+
+
+
+
     </div>
+
+    
+   
+
   );
 }
 
